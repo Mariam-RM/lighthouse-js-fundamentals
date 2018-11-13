4f215d51a6d68033e48704d54ad0297e2e8b5335
Mariam-RM <mariam.majeed.kes@gmail.com>
2018-11-13 00:56:12 +0000

    Adds conditional exercises

---
+++ go-outside.js
@@ -0,0 +1,37 @@
+// var raining = true;
+// var cold = false;
+
+// if(raining){
+//   console.log("Don't forget your umbrella");
+// }
+
+// if(cold){
+// console.log("Make sure you pick out a scarf!");
+// }
+
+// console.log("Now you're ready to go outside!");
+
+
+// part 2:
+
+// var cold = false;
+
+// if (cold){
+//   console.log("Make sure you pick out a scarf!");
+// } else {
+//   console.log("Short sleeves are fine.");
+// }
+
+// part 3:
+
+var temperature = 12;
+
+if (temperature < 0){
+  console.log("Make sure you pick out a scarf!");
+} else if (temperature < 15) {
+  console.log ("Short sleeves won't cut it!");
+} else {
+  console.log("Short slevees are fine.");
+}
+
+console.log("Now you're ready to go outside!");
